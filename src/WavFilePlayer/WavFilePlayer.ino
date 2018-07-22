a7d4f55f5f2610a925e799538f3b26759bf99d56
Chris Veigl <veigl@technikum-wien.at>
2018-07-22 22:34:56 +0200

    some cleanup ...

--- src/WavFilePlayer/WavFilePlayer.ino
+++ src/WavFilePlayer/WavFilePlayer.ino
@@ -1,27 +1,6 @@
-// Simple WAV file player example
-//
-// Three types of output may be used, by configuring the code below.
-//
-//   1: Digital I2S - Normally used with the audio shield:
-//         http://www.pjrc.com/store/teensy3_audio.html
-//
-//   2: Digital S/PDIF - Connect pin 22 to a S/PDIF transmitter
-//         https://www.oshpark.com/shared_projects/KcDBKHta
-//
-//   3: Analog DAC - Connect the DAC pin to an amplified speaker
-//         http://www.pjrc.com/teensy/gui/?info=AudioOutputAnalog
-//
-// To configure the output type, first uncomment one of the three
-// output objects.  If not using the audio shield, comment out
-// the sgtl5000_1 lines in setup(), so it does not wait forever
-// trying to configure the SGTL5000 codec chip.
-//
-// The SD card may connect to different pins, depending on the
-// hardware you are using.  Uncomment or configure the SD card
-// pins to match your hardware.
-//
-// Data files to put on your SD card can be downloaded here:
-//   http://www.pjrc.com/teensy/td_libs_AudioDataFiles.html
+// WAV file player for Gaston
+// based on simple wav player demo by PJRC, see:
+//   https://www.pjrc.com/teensy/td_libs_Audio.html
 //
 // This example code is in the public domain.
 
@@ -32,10 +11,7 @@
 #include <SerialFlash.h>
 
 AudioPlaySdWav           playWav1;
-// Use one of these 3 output types: Digital I2S, Digital S/PDIF, or Analog DAC
 AudioOutputI2S           audioOutput;
-//AudioOutputSPDIF       audioOutput;
-//AudioOutputAnalog      audioOutput;
 AudioConnection          patchCord1(playWav1, 0, audioOutput, 0);
 AudioConnection          patchCord2(playWav1, 1, audioOutput, 1);
 AudioControlSGTL5000     sgtl5000_1;
@@ -49,13 +25,8 @@ void setup() {
   Serial.begin(9600);
   Serial1.begin(9600);
 
-  // Audio connections require memory to work.  For more
-  // detailed information, see the MemoryAndCpuUsage example
   AudioMemory(8);
 
-  // Comment these out if not using the audio adaptor board.
-  // This may wait forever if the SDA & SCL pins lack
-  // pullup resistors
   sgtl5000_1.enable();
   sgtl5000_1.volume(0.2);
 
@@ -75,29 +46,15 @@ void playFile(const char *filename)
   Serial.print("Playing file: ");
   Serial.println(filename);
 
-  // Start playing the file.  This sketch continues to
-  // run while the file plays.
   playWav1.play(filename);
-
   // A brief delay for the library read WAV info
   delay(5);
-
-  // Simply wait for the file to finish playing.
-  //  while (playWav1.isPlaying()) {}
 }
 
 char filename[] = "x.wav";
 
 void loop() {
   int incomingByte;
-/*
-  playFile("1.wav");  // filenames are always uppercase 8.3 format
-  while (1) {
-    float vol = analogRead(15);
-    vol = vol / 3000;
-    sgtl5000_1.volume(vol);
-  }
-*/
 
   float vol = analogRead(15);
   vol = vol / 3000;
